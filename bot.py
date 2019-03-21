cbc8f7fadce9216e13beb51900bd36105e4f31e3
Bilashos <36623574+Bilashos@users.noreply.github.com>
2019-03-21 14:34:30 +0100

    aggiornamento numero 2

--- bot.py
+++ bot.py
@@ -1,15 +1,21 @@
 import telepot
 import csv
 import math
-from config import TOKEN
+from config import TOKEN, API_KEY
 from telepot.namedtuple import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
 import bs4 as bs
 import urllib.request
 import json
 from pprint import pprint
 import time
+from apiclient.discovery import build
+import requests
+import gmaps
 
+dati1 =[()]
+dati2 =[()]
 dati3 =[()]
+dati4 =[]
 user_state = 0
 def handle(msg):
 #########################################
@@ -17,11 +23,15 @@ def handle(msg):
 #                    (self.USER_STATE[chat_id] == 1 or self.USER_STATE[chat_id] == 2):
 #       self.USER_STATE[chat_id] = (self.USER_STATE[chat_id] * 10) + 2
 #########################################
+    global dati1
+    global dati2
     global dati3
     global user_state
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type == "text":
+        msgg = json.dumps(msg, indent=4)
         input_msg = msg['text']
+        print(msgg)
         if input_msg == "/inizia" and \
             user_state == 0:
             entries = [["Linea", "Fermata", "Via"]]
@@ -38,9 +48,9 @@ def handle(msg):
                 lettore = csv.reader(filecsv,delimiter=",")
                 #filtro dati adriabus che
                 dati = [(riga[0]) for riga in lettore if riga[1] == "ADRIABUS" and riga[2] == routes_search]
-                print(dati)
+                #print(dati)
                 route_id = dati[0]
-                print(route_id)
+                #print(route_id)
                 #filtra file trips.txt ricavi trip_id
             with open("./trips.txt", newline="") as filecsv:
                 lettore = csv.reader(filecsv,delimiter=",")
@@ -50,13 +60,14 @@ def handle(msg):
         #stoptimes.txt stop_id
             with open("./stop_times.txt", newline="") as filecsv:
                 lettore = csv.reader(filecsv,delimiter=",")
-                dati2 = [(riga[3]) for riga in lettore if (riga[0] in dati1)]
+                dati2 = [(riga[0],riga[1],riga[2],riga[3]) for riga in lettore if (riga[0] in dati1)]
                 print(dati2)
+                dati22 =[(value[3])for value in dati2]
+
         #stop_id stops.txt prendo latitudine e longitudine
             with open("./stops.txt", newline="") as filecsv:
                 lettore = csv.reader(filecsv,delimiter=",")
-                global dati3
-                dati3 = [(riga[2],riga[3],riga[1]) for riga in lettore if (riga[0] in dati2)]
+                dati3 = [(riga[2],riga[3],riga[1],riga[0]) for riga in lettore if (riga[0] in dati22)]
                 print(dati3)
         #richiedi posizione e verifica quella più vicino di lista 3
         #bot.sendMessage(chat_id, "Mandami la tua posizione per la fermata più vicina!")
@@ -83,8 +94,46 @@ def handle(msg):
                         fermata_giusta = dato
 
 
-            bot.sendMessage(chat_id, str(fermata_giusta))
+
+
+            origins = str(latitude)+","+str(longitude)
+            destinations = str(float(fermata_giusta[0]))+","+str(float(fermata_giusta[1]))
+            mode = 'walking'
+
+            URL = str("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+str(origins)+"&destinations="+str(destinations)+"&mode="+mode+"&key="+API_KEY)
+            r = requests.get(url = URL)
+            data = r.json()
+            #data = json.dumps(data, indent=4)
+            #data = json.dumps(r.json(), indent=2, sort_keys=True)
+
+            print(data["rows"])
+            distance = data["rows"][0]["elements"][0]["distance"]["text"]
+            duration = data["rows"][0]["elements"][0]["duration"]["text"]
+            #filehandle = urllib.request.urlopen(URL)
+            global dati4
+            for dato in dati2:
+                for dato1 in dati1:
+                    if(dato1 in dato[0] and fermata_giusta[3] in dato[3]):
+                        dati4.append(dato[1])
+
+            print(dati4)
+
+            # i = 0
+            # for dato in dati1:
+            #     if(dati1[i] in dati2 and feramata_giusta[3] in dati2):
+            #         dati4.append(dato)
+            #     if(i+1 < len(dati1)):
+            #         i = i+1
+            #
+            # print (dati4)
+            #destinations: San+Francisco|Victoria+BC
+            bot.sendLocation(chat_id, float(fermata_giusta[0]),float(fermata_giusta[1]))
+            bot.sendMessage(chat_id, str(fermata_giusta)+" dista: "+distance+" tempo a piedi: "+duration)
+            #print(distance)
+            bot.sendMessage(chat_id, str(dati4))
             user_state = 0
+            #mode: driving
+            #key: API_KEY
     else:
         entries = [["Pranzo"], ["Cena"]]
         markup = ReplyKeyboardMarkup(keyboard=entries)
