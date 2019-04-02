7c531dcfa4db4f15b0ca44aaa299de3dc0584e47
Bilashos <36623574+Bilashos@users.noreply.github.com>
2019-04-02 14:54:01 +0200

    aggiornamento bot
    messo listino prezzi corse extraUrbane

--- bot.py
+++ bot.py
@@ -14,8 +14,10 @@ import gmaps
 import time
 import os
 import mysql.connector
+from emoji import emojize
 
-
+lista_costi = []
+lista_descrizione = []
 dati1 =[()]
 dati2 =[()]
 dati3 =[()]
@@ -24,14 +26,13 @@ count = 0
 count2 = 0
 user_state = 0
 myresult = []
+cake = emojize(":cake:", use_aliases=True)
 def handle(msg):
+
     cnx = mysql.connector.connect(user='root', password='doremi666', host='localhost', database='mydb')
     mycursor = cnx.cursor()
-#########################################
-#elif input_msg == "Classico" and \
-#                    (self.USER_STATE[chat_id] == 1 or self.USER_STATE[chat_id] == 2):
-#       self.USER_STATE[chat_id] = (self.USER_STATE[chat_id] * 10) + 2
-#########################################
+    global lista_descrizione
+    global lista_costi
     global dati1
     global dati2
     global dati3
@@ -39,17 +40,27 @@ def handle(msg):
     global myresult
     global count
     global count2
+    global dati4
     content_type, chat_type, chat_id = telepot.glance(msg)
+    keyboardd = InlineKeyboardMarkup(inline_keyboard=[
+                     [InlineKeyboardButton(text='Yes', callback_data='yes'),
+                     InlineKeyboardButton(text='No', callback_data='no')],
+                 ])
+
+
     if content_type == "text":
         msgg = json.dumps(msg, indent=4)
         input_msg = msg['text']
         print(msgg)
         if input_msg == "/inizia" and \
             user_state == 0:
-            entries = [["Linea", "Fermata", "Via"]]
+            entries = [["Linea", "Fermata", "Prezzi"]]
             markup = ReplyKeyboardMarkup(keyboard=entries)
-            bot.sendMessage(chat_id,'Scegli l''opzione',reply_markup=markup)
+            bot.sendMessage(chat_id,'Scegli l\'opzione',reply_markup=markup)
             user_state = 1
+        elif input_msg == "/start" and \
+            user_state == 0:
+            bot.sendMessage(chat_id, "Benvenuto "+msg['chat']['first_name']+" questo bot è inutile al cazzo l\'abbiamo fatto solo per un esame del cazzo e questo è il risultato, non criticare grazie! o se proprio hai da criticare critica @wepesarobot")
         elif msg['text'] == "Linea" and \
             user_state == 1:
             bot.sendMessage(chat_id, 'Digita la linea che vuoi cercare...')
@@ -58,24 +69,36 @@ def handle(msg):
 
             bot.sendMessage(chat_id, "Ricerca linea...")
             routes_search = msg['text'].replace('/','')
-            # with open("./routes.txt", newline="") as filecsv:
-            #     lettore = csv.reader(filecsv,delimiter=",")
-            #     #filtro dati adriabus che
-            print("SELECT routes.route_id from routes where routes.agency_id = 'ADRIABUS' and routes.route_short_name = "+routes_search+"")
+            #print("SELECT routes.route_id from routes where routes.agency_id = 'ADRIABUS' and routes.route_short_name = "+routes_search+"")
             mycursor.execute("SELECT DISTINCT stop_times.stop_id from routes, trips, stop_times where routes.agency_id = 'ADRIABUS' and routes.route_short_name = \'"+str(routes_search)+"\' and  trips.route_id = routes.route_id and stop_times.trip_id = trips.trip_id")
             myresult = mycursor.fetchall()
             bot.sendMessage(chat_id, "Mandami la tua posizione per la fermata più vicina!")
             user_state = 3
+
         elif msg['text'] == "Fermata" and \
             user_state == 1:
-            mycursor.execute("SELECT DISTINCT routes.route_short_name from routes where routes.agency_id = 'ADRIABUS'")
+            entries = [["Pesaro", "Fano"],["Urbino", "Di Quartiere"],["SCOLASTICA"]]
+            markup = ReplyKeyboardMarkup(keyboard=entries)
+            bot.sendMessage(chat_id,'Scegli l\'opzione',reply_markup=markup)
+            user_state = 1.22
+        elif user_state == 1.22:
+            if msg['text'] == "Pesaro":
+                mycursor.execute("SELECT DISTINCT routes.route_short_name, routes.route_long_name from routes where routes.agency_id = 'ADRIABUS' and (routes.route_short_name LIKE 'P%' or routes.route_long_name like '%PESARO%')")
+            elif msg['text'] == "Fano":
+                mycursor.execute("SELECT DISTINCT routes.route_short_name, routes.route_long_name from routes where routes.agency_id = 'ADRIABUS' and routes.route_short_name LIKE 'F%' or routes.route_long_name like '%FANO%'")
+            elif msg['text'] == "Urbino":
+                mycursor.execute("SELECT distinct routes.route_short_name, routes.route_long_name from routes where routes.agency_id = 'ADRIABUS' and routes.route_short_name LIKE 'U%' or routes.route_long_name like '%URBINO%'")
+            elif msg['text'] == "Di Quartiere":
+                mycursor.execute("SELECT distinct routes.route_short_name, routes.route_long_name from routes where routes.agency_id = 'ADRIABUS' and routes.route_short_name NOT LIKE 'F%' and routes.route_short_name NOT LIKE 'U%' and routes.route_short_name NOT LIKE 'P%'")
+            elif msg['text'] == "SCOLASTICA":
+                mycursor.execute("SELECT distinct routes.route_short_name, routes.route_long_name from routes where routes.agency_id = 'ADRIABUS' and routes.route_long_name like '%SCOLASTICA%'")
             myresult = mycursor.fetchall()
             i = 0
             count = 10
             print(len(myresult))
             for x in myresult:
                 if count2 <= count:
-                    bot.sendMessage(chat_id, "/"+myresult[count2][0])
+                    bot.sendMessage(chat_id, "/"+myresult[count2][0]+" "+myresult[count2][1])
                     count2 = i
                 elif i == (len(myresult) - 1):
                     bot.sendMessage(chat_id, "/other")
@@ -96,12 +119,86 @@ def handle(msg):
 
                 i = i+1
                 user_state = 5
+        elif msg['text'] == "Prezzi" and \
+            user_state == 1:
+                entries = [["ExtraUrbane", "Urbane"]]
+                markup = ReplyKeyboardMarkup(keyboard=entries)
+                bot.sendMessage(chat_id,'Scegli l\'opzione',reply_markup=markup)
+                user_state = 1.2
+
+        elif user_state == 1.2 and msg['text'] == "Urbane":
+            entries = [["Pesaro", "Fano"],["Urbino"]]
+            markup = ReplyKeyboardMarkup(keyboard=entries)
+            bot.sendMessage(chat_id,'Scegli l\'opzione',reply_markup=markup)
+        elif user_state == 1.3 or msg['text'] == "ExtraUrbane":
+            if msg['text'] == "ExtraUrbane":
+                URL = "http://www.adriabus.eu/viewdoc.asp?co_id=7703"
+                try:
+                    sauce = urllib.request.urlopen(URL).read()
+                    soup = bs.BeautifulSoup(sauce, 'html.parser')
+                        #cercare i tag span con classe abuot stat
+                    i= 0
+                    j = 0
+                    for item in soup.find_all('p'):
+                        if j != 0 and j != -5:
+                            answer = ''.join(item.text)
+                            #print(answer+" "+str(i))
+
+                            if j < 5 and j != 0:
+                                lista_descrizione.append(answer)
+                                j = j+1
+                            elif answer == '3,70':
+                                j = -5
+                                lista_costi.append(answer)
+                            else:
+                                lista_costi.append(answer)
+                        elif i == 5 and j == 0:
+                            i = -1
+                            j = j+1
+
+                            #fascia.append(lista_costi)
+                        i = i+1
+                            #bot.sendMessage(chat_id, answer)
+                           #bot.sendMessage(chat_id, item.text)
+                except urllib.error.HTTPError as err:
+                    if err.code == 404:
+                        bot.sendMessage(chat_id, msg)
+                i = 0
+                testo = ''
+                for dato in lista_costi:
+                    if i < 3:
+                        testo = testo+dato+ " | "
+                    elif i < 7 and i >= 3:
+                        print(dato)
+                    else:
+                        testo = testo+"\n-----------------------\n"+dato+ " | "
+                        i = 0
+                    # dati4 = []
+                    i = i+1
+                    print(dato)
+
+                bot.sendMessage(chat_id, testo)
+                bot.sendMessage(chat_id, "Vuoi scoprire la distanza della tua tratta?", reply_markup=keyboardd)
+            # elif msg['text'] == "Pesaro":
+            #     URL = "http://www.adriabus.eu/viewdoc.asp?co_id=7696"
+            # elif msg['text'] == "Fano" or msg['text'] == "Urbino":
+            #     URL = "http://www.adriabus.eu/viewdoc.asp?co_id=7699"
+            # try:
+            #     sauce = urllib.request.urlopen(URL).read()
+            #     soup = bs.BeautifulSoup(sauce, 'html.parser')
+            #         #cercare i tag span con classe abuot stat
+            #     for item in soup.find_all('p', 'MsoNormal'):
+            #         answer = ''.join(item.text)
+            #         print(answer)
+            #             #bot.sendMessage(chat_id, answer)
+            #            #bot.sendMessage(chat_id, item.text)
+            # except urllib.error.HTTPError as err:
+            #     if err.code == 404:
+            #         bot.sendMessage(chat_id, msg)
 
 
 
 
-
-#stop_id stops.txt prendo latitudine e longitudine
     elif content_type == 'location' and \
             user_state == 3:
             latitude = float(msg["location"]["latitude"])
@@ -121,42 +218,18 @@ def handle(msg):
                     if temp < min:
                         min = temp
                         fermata_giusta = newresult
-
-
             origins = str(latitude)+","+str(longitude)
             destinations = str(float(fermata_giusta[0][0]))+","+str(float(fermata_giusta[0][1]))
             mode = 'walking'
-
             URL = str("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+str(origins)+"&destinations="+str(destinations)+"&mode="+mode+"&key="+API_KEY)
             r = requests.get(url = URL)
             data = r.json()
-            #data = json.dumps(data, indent=4)
-            #data = json.dumps(r.json(), indent=2, sort_keys=True)
-
             print(data["rows"])
             distance = data["rows"][0]["elements"][0]["distance"]["text"]
             duration = data["rows"][0]["elements"][0]["duration"]["text"]
-            #filehandle = urllib.request.urlopen(URL)
-            # global dati4
-            # for dato in dati2:
-            #     for dato1 in dati1:
-            #         if(dato1 in dato[0] and fermata_giusta[3] in dato[3]):
-            #             dati4.append(dato[1])
-            #
-            # print(dati4)
-
-            # i = 0
-            # for dato in dati1:
-            #     if(dati1[i] in dati2 and feramata_giusta[3] in dati2):
-            #         dati4.append(dato)
-            #     if(i+1 < len(dati1)):
-            #         i = i+1
-            #
-            # print (dati4)
-            #destinations: San+Francisco|Victoria+BC
             bot.sendLocation(chat_id, float(fermata_giusta[0][0]),float(fermata_giusta[0][1]))
             bot.sendMessage(chat_id, str(fermata_giusta)+" dista: "+distance+" tempo a piedi: "+duration)
-
+            user_state = 0
 
 
 
@@ -288,22 +361,6 @@ def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print("Callback Query: ", query_id, from_id, query_data)
     bot.answerCallbackQuery(query_id, text="YEAH")
-
-
-
-#bot = telepot.Bot(TOKEN)
-#response = bot.getUpdates()
-
-
-#pprint(response)
-#bot.message_loop({'chat': on_chat_message,
-#                  'callback_query': on_callback_query})
-
-#eyboard = InlineKeyboardMarkup(inline_keyboard=[
-#                     [InlineKeyboardButton(text='IP', callback_data='ip'),
-#                     InlineKeyboardButton(text='Info', callback_data='info'),
-#                     InlineKeyboardButton(text='Time', callback_data='time')],
-#                 ])
 
 bot = telepot.Bot(TOKEN)
 bot.message_loop({'chat':handle,
