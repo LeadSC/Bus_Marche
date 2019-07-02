73d1f061ad462eb021926c3dd27264df4a7b66c4
Bilashos <36623574+Bilashos@users.noreply.github.com>
2019-07-02 22:39:50 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 TOKEN = '586837575:AAGJz-NWxf-OHSpqAk_Fs7NN9dFEBgFRpf4'
 API_KEY = 'AIzaSyBHIf6PCXfTPy4EumP_ZjG4g5C1xXN6EeQ'
+URL_EXTRAURBANE = "http://www.adriabus.eu/viewdoc.asp?co_id=7703"
+URL_URBANE = "http://www.adriabus.eu/viewdoc.asp?co_id=7696"
